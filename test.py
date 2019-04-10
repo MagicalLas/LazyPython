bc7cc08af71f97970985013b2c7469b40ed4f326
Wonho ha <haho013039@gmail.com>
2019-04-10 14:31:48 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,52 @@
+import main
+
+
+@main.lazy
+def sum(a, b):
+    return a + b
+
+
+def mul(a):
+    @main.lazy
+    def lazy_mul(b):
+        return a * b
+    return lazy_mul
+
+
+def test_lazy_sum():
+    sum_effect = sum(1, 2)
+    assert sum_effect.excute() == 1 + 2
+    assert not sum_effect.excute() == 1 + 3
+
+    assert isinstance(sum_effect, main.Effect)
+
+
+def test_effect_chain():
+    sum_effect = sum(1, 2)
+    print_effect = sum_effect.map(print)
+
+    assert isinstance(print_effect, main.ChainEffect)
+
+
+def test_lazy_one_multiplex():
+    multiplexer = mul(2)
+    sum_effect = sum(1, 2)
+
+    result_effect = sum_effect\
+        .map(multiplexer)
+
+    assert isinstance(result_effect, main.ChainEffect)
+    assert result_effect.excute() == 3 * 2
+
+
+def test_lazy_multiplex():
+    multiplexer = mul(2)
+    sum_effect = sum(1, 2)
+
+    result_effect = sum_effect\
+        .map(multiplexer)\
+        .map(multiplexer)\
+        .map(multiplexer)
+
+    assert isinstance(result_effect, main.ChainEffect)
+    assert result_effect.excute() == 3 * 2 * 2 * 2
