b9f92451acd2325a3cf215bd944155cd4aac37b7
Wonho ha <haho013039@gmail.com>
2019-04-16 12:22:11 +0900

    Update utils.py

--- lazy/utils.py
+++ lazy/utils.py
@@ -4,7 +4,13 @@ _print = print
 _input = input
 
 _int = int
+_chr = chr
+_bool = bool
 _string = string
+
+_abs = abs
+_all = all
+_any = any
 
 
 @lazy
@@ -16,10 +22,35 @@ def print(*arg, **kwargs):
 def input(*arg, **kwargs):
     return _input(*arg, **kwargs)
 
+
 @lazy
 def int(*arg, **kwargs):
     return _int(*arg, **kwargs)
 
 @lazy
+def chr(*arg, **kwargs):
+    return _chr(*arg, **kwargs)
+
+@lazy
+def bool(*arg, **kwargs):
+    return _bool(*arg, **kwargs)
+
+
+@lazy
 def string(*arg, **kwargs):
     return _string(*arg, **kwargs)
+
+
+@lazy
+def abs(*arg, **kwargs):
+    return _abs(*arg, **kwargs)
+
+
+@lazy
+def all(*arg, **kwargs):
+    return _all(*arg, **kwargs)
+
+
+@lazy
+def any(*arg, **kwargs):
+    return _any(*arg, **kwargs)
