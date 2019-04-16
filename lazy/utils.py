6058fac7c0e02e9ebf5ed13cb3212153ae86bb6f
Wonho ha <haho013039@gmail.com>
2019-04-16 12:12:35 +0900

    Update utils.py

--- lazy/utils.py
+++ lazy/utils.py
@@ -3,6 +3,9 @@ from .lazy import lazy
 _print = print
 _input = input
 
+_int = int
+_string = string
+
 
 @lazy
 def print(*arg, **kwargs):
@@ -12,3 +15,11 @@ def print(*arg, **kwargs):
 @lazy
 def input(*arg, **kwargs):
     return _input(*arg, **kwargs)
+
+@lazy
+def int(*arg, **kwargs):
+    return _int(*arg, **kwargs)
+
+@lazy
+def string(*arg, **kwargs):
+    return _string(*arg, **kwargs)
