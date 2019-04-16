9c348ba45183cdeb9436e3eed630d6d9fc06d873
Wonho ha <haho013039@gmail.com>
2019-04-16 20:14:06 +0900

    Update add_two_number.py

--- example/add_two_number.py
+++ example/add_two_number.py
@@ -5,11 +5,13 @@ from lazy.ef_app import EfApp
 
 @lazy
 def sum(a, b):
-    return a + b
+    if a.right or b.right:
+        return 0
+    return a.left + b.left
 
 @EfApp
 def app():
-    number_a = input("input number 1 : ") << int
-    number_b = input("input number 2 : ") << int
+    number_a = (input("input number 1 : ") << int).attempt()
+    number_b = (input("input number 2 : ") << int).attempt()
     app_effect = sum(number_a.excute(), number_b.excute()) << print
     return app_effect
