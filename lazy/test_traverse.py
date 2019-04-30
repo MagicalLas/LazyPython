9dc228a718a25bc9bc0f30f4be2e3675733fad5b
Wonho ha <haho013039@gmail.com>
2019-04-30 17:39:51 +0900

    [CREATE] traverse test case

---
+++ lazy/test_traverse.py
@@ -0,0 +1,12 @@
+from .effect import traverse, pure, Effect
+
+def test_pure():
+    p = pure("Lazy")
+    assert isinstance(p, Effect)
+    assert p.excute() == "Lazy"
+
+def test_traverse():
+    effects = [pure(5), pure(-5), pure(3), pure(0)]
+    effect_t = traverse(effects)
+    assert isinstance(effect_t, Effect)
+    assert effect_t.excute() == [5, -5, 3, 0]
