ad2d1f9f37712584b9b9384649f230418bb21a28
Wonho ha <haho013039@gmail.com>
2019-04-30 23:46:02 +0900

    [REFACTOR] effects test case

--- lazy/test_lazy_effect.py
+++ lazy/test_lazy_effect.py
@@ -1,40 +1,15 @@
-from .effect import lazy, Effect
-
-
-@lazy
-def sum(a, b):
-    return a + b
-
-
-def mul(a):
-    @lazy
-    def lazy_mul(b):
-        return a * b
-    return lazy_mul
+from .effect import lazy, Effect, pure
+from .typematcher import _type, TypeMatcher, chain
+from .either import Left, Right
 
 
 def test_lazy_sum():
-    sum_effect = sum(1, 2)
-    assert isinstance(sum_effect, Effect)
-    assert sum_effect.excute() == 1 + 2
-    assert not sum_effect.excute() == 1 + 3
+    mather = TypeMatcher(
+        _type(Left) < - chain(lambda x: True),
+        _type(Right) < - chain(lambda x: False),
+    )
 
-
-def test_lazy_maps():
-    sum_effect = sum(1, 2)
-    result = sum_effect\
-        .map(lambda x: x + 1)\
-        .map(lambda x: x + 2)\
-        .map(lambda x: x + 3)\
-        .excute()
-    assert result == 1 + 2 + 1 + 2 + 3
-
-
-def test_lazy_one_multiplex():
-    multiplexer = mul(2)
-    sum_effect = sum(1, 2)
-    chain_effect = sum_effect\
-        .flatMap(multiplexer)\
-        .flatMap(multiplexer)\
-        & (lambda x: x - 4)
-    assert chain_effect.excute() == (3 * 2 * 2) - 4
+    effect = pure(4)
+    effect = effect << lazy(lambda x: x / 0)
+    effect = effect.attempt()
+    assert (effect & mather).excute() is False
