743afb86af19c8b91cdacaa7de00b877810412c9
Wonho ha <haho013039@gmail.com>
2019-04-30 23:46:42 +0900

    [CREATE] resource test case

---
+++ lazy/test_resource.py
@@ -0,0 +1,11 @@
+from .effect import pure, lazy
+from .utils import input
+from .resource import Resource
+
+def test_resource(monkeypatch):
+    monkeypatch.setattr('lazy.utils._input', lambda x: "Lazy")
+
+    resource_effect = input("Get Resource")
+    resource = Resource.make(resource_effect)(lazy(lambda x: x))
+    resource = resource.use(lambda x:x+"~!").map(lambda x:"Hello "+x).flatMap(lazy(lambda x: x))
+    assert resource.excute() == "Hello Lazy~!"
