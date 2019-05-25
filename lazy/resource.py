428cf501577affcf14cea6be6749d6830168a09e
Wonho ha <haho013039@gmail.com>
2019-05-25 14:42:54 +0900

    Delete All

--- lazy/resource.py
+++ lazy/resource.py
@@ -3,36 +3,18 @@ from .effect import lazy
 
 class Resource(object):
 
-    def __init__(self, f, g, middle):
-        self.f = f
-        self.g = g
-        self.middle = middle
 
     def use(self, executer):
-        @lazy
-        def dummy():
-            self.file = self.f.execute
-            return executer(self.file)
-        self.middle = dummy()
-        return self
 
     def map(self, f):
-        self.middle = self.middle & f
-        return self
 
     def flatMap(self, f):
-        self.middle = self.middle << f
-        return self
 
     @property
     def attempt(self):
-        self.middle = self.middle.attempt
-        return self
 
     @property
     def execute(self):
-        result = self.middle.execute
-        self.g(self.file)
         return result
 
     @staticmethod
